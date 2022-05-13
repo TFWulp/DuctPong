cd8784913123a424167a206596675b2a234eb4e4
Timo <t.f.vanderwulp@students.uu.nl>
2022-05-13 17:05:54 +0200

    NPM Package initialization

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Pixi.js Demo'
+    }),
+    new CopyPlugin({
+        patterns: [
+            { from: 'src/assets', to: 'assets' },
+        ]
+    })
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
